b4323c2dfa1b5dcf5ff5d4363eaee40af9d97201
Maxim Fursov <maxick.95@mail.ru>
2021-11-17 13:23:56 +0300

    Урок 1 с 6 заданиями

---
+++ ex3.py
@@ -0,0 +1,6 @@
+n1 = input('Введите число: ')
+n2 = n1+n1
+n3 = n2+n1
+print(n1, n2, n3)
+result = int(n1)+int(n2)+int(n3)
+print(result)
---
+++ ex4.py
@@ -0,0 +1,8 @@
+chislo = int(input('Введите целое положительное число: '))
+maxcifra = chislo%10
+chislo //=10
+while chislo > 0:
+    if chislo%10 > maxcifra:
+        maxcifra = chislo%10
+    chislo //=10
+print('Максимальная цифра - ', maxcifra)
